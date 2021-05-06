705174df1d91ee34fbff79ba1547a0be3148b966
Eugenio Tampieri <eugenio.tampieri@studio.unibo.it>
2021-05-06 11:19:44 +0200

    WIP Action

---
+++ Tampieri/Tampieri/Action.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tampieri
+{
+    public enum Action
+    {
+        Move,
+        Attack,
+        Idle,
+        Die,
+        
+    }
+    public static class ActionExtension
+    {
+        public static string ToString(this Action a)
+        {
+            return a.ToString().ToLower();
+        }
+
+        public static Action ToAction(this String s)
+        {
+            return s switch
+            {
+                "move" => Action.Move,
+                "attack" => Action.Attack,
+                "idle" => Action.Idle,
+                "die" => Action.Die,
+                _ => throw new InvalidCastException(),
+            };
+        }
+    }
+}
