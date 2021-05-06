0c9a6b24d7bcf047b3db48520bf5f002b6f97bb1
Eugenio Tampieri <eugenio@eutampieri.eu>
2021-05-06 22:32:09 +0200

    Test player

--- Tampieri/TestTampieri/LivingCharactersTest.cs
+++ Tampieri/TestTampieri/LivingCharactersTest.cs
@@ -6,12 +6,45 @@ namespace TestTampieri
     [TestClass]
     public class LivingCharactersTest
     {
-        private Player PLAYER = new Player("Jane Appleseed");
+        private ILivingCharacter PLAYER = new Player("Jane Appleseed");
         [TestMethod]
         public void TestMovements()
         {
-            Assert.IsTrue(PLAYER.CanPerform(Action.Move));
-            Assert.IsFalse(PLAYER.CanPerform(Action.Idle));
+            Assert.IsTrue(new Player("Jane Appleseed").CanPerform(Action.Move));
+            Assert.IsFalse(new Player("Jane Appleseed").CanPerform(Action.Idle));
+        }
+
+        [TestMethod]
+        public void TestPotion()
+        {
+            IHealthModifier badPotion = new SimplePotion("Bad potion", -20);
+            IHealthModifier goodPotion = new SimplePotion("GoodPotion", 15);
+            badPotion.UseOn(PLAYER);
+            goodPotion.UseOn(PLAYER);
+            Assert.AreEqual(PLAYER.Health, 95);
+        }
+
+        [TestMethod]
+        public void TestHealthOverflow()
+        {
+            IHealthModifier goodPotion = new SimplePotion("GoodPotion", 20);
+            for (int i = 0; i < 6; i++)
+            {
+                goodPotion.UseOn(PLAYER);
+            }
+            Assert.AreEqual(PLAYER.Health, 100);
+        }
+
+        [TestMethod]
+        public void TestHealthUnderflow()
+        {
+            IHealthModifier badPotion = new SimplePotion("Bad potion", -20);
+            for (int i = 0; i < 6; i++)
+            {
+                badPotion.UseOn(PLAYER);
+            }
+            Assert.AreEqual(PLAYER.Health, 0);
+            Assert.IsTrue(PLAYER.IsAlive());
         }
     }
 }
