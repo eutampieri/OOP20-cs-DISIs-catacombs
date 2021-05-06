035ed6ab7766a6ea358d6c6872333a5d215a9164
Eugenio Tampieri <eugenio@eutampieri.eu>
2021-05-06 14:59:31 +0200

    Implemented ImageTransformer

---
+++ Tampieri/Tampieri/IImageTransformerFactory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SixLabors.ImageSharp;
+
+namespace Tampieri
+{
+    interface IImageTransformerFactory
+    {
+        Func<Image, Image> rotate(double degrees);
+        Func<Image, Image> scale(double factor);
+        Func<Image, Image> flip(bool flipX, bool flipY);
+    }
+}
---
+++ Tampieri/Tampieri/ImageTransformerFactory.cs
@@ -0,0 +1,53 @@
+﻿using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Processing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tampieri
+{
+    class ImageTransformerFactory : IImageTransformerFactory
+    {
+        public Func<Image, Image> flip(bool flipX, bool flipY)
+        {
+            return image =>
+            {
+                Image result = image;
+                if (flipX)
+                {
+                    result.Mutate(x => x.Flip(FlipMode.Vertical));
+                }
+                if (flipY)
+                {
+                    result.Mutate(x => x.Flip(FlipMode.Horizontal));
+                }
+                return result;
+            };
+        }
+
+        public Func<Image, Image> rotate(double degrees)
+        {
+            return image =>
+            {
+                Image result = image;
+                result.Mutate(x => x.Rotate((float)degrees));
+                return result;
+            };
+        }
+
+        public Func<Image, Image> scale(double factor)
+        {
+                return image =>
+                {
+                    int finalWidth = (int)(image.Width * factor);
+                    int finalHeight = (int)(image.Height * factor);
+                    Image result = image;
+                    result.Mutate(x => x.Resize(finalWidth, finalHeight));
+                    return result;
+                };
+            }
+        }
+    }
+}
