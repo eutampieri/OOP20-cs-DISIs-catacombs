4e2137930aaa30a941ef2d29b583a19c7d067607
Eugenio Tampieri <eugenio.tampieri@studio.unibo.it>
2021-05-06 11:32:54 +0200

    Implemented LivingCharacter and HealthModifier

---
+++ Tampieri/Tampieri/IHealthModifier.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tampieri
+{
+    interface IHealthModifier
+    {
+        int GetHealthDelta();
+        void UseOn(ILivingCharacter character)
+        {
+            int currentHealth = character.GetCurrentHealth();
+            currentHealth += this.GetHealthDelta();
+            character.SetHealth(currentHealth);
+        }
+        string ToString();
+    }
+}
---
+++ Tampieri/Tampieri/ILivingCharacter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tampieri
+{
+    interface ILivingCharacter
+    {
+        int GetCurrentHealth();
+        void SetHealth(int health);
+        bool IsAlive()
+        {
+            return this.GetCurrentHealth() > 0;
+        }
+    }
+}
