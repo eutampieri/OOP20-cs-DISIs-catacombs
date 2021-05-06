a60e49981e1a111c138cdceec07608a7d633771c
Eugenio Tampieri <eugenio.tampieri@studio.unibo.it>
2021-05-06 11:23:01 +0200

    Implemented Direction

---
+++ Tampieri/Tampieri/Direction.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tampieri
+{
+    public enum Direction
+    {
+        Up,
+        Down,
+        Left,
+        Right,
+    }
+
+    public static class DirectionExtentions
+    {
+        public static string ToString(this Direction d)
+        {
+            return d switch
+            {
+                Direction.Up => "up",
+                Direction.Right => "right",
+                Direction.Down => "down",
+                Direction.Left => "left",
+            };
+        }
+    }
+}
