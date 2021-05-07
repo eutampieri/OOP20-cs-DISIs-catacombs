3e1bfb64c7997b50c61e388a8ff89b0b3fedb2e1
Eugenio Tampieri <eugenio@eutampieri.eu>
2021-05-07 09:54:20 +0200

    Documented ILivingCharacter

--- Tampieri/Tampieri/ILivingCharacter.cs
+++ Tampieri/Tampieri/ILivingCharacter.cs
@@ -1,5 +1,6 @@
 ﻿namespace Tampieri.Model
 {
+    /// <summary>Implement this when you want an entity to possess the concept of health and life</summary>
     public interface ILivingCharacter
     {
         int Health { get; set; }
