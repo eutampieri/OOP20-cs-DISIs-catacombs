706fc4c28d16c76e4012eaccb350189e82ff8205
Eugenio Tampieri <eugenio@eutampieri.eu>
2021-05-07 09:50:26 +0200

    Documented IAnimatable

--- Tampieri/Tampieri/IAnimatable.cs
+++ Tampieri/Tampieri/IAnimatable.cs
@@ -2,8 +2,10 @@
 
 namespace Tampieri.Ui
 {
+    /// <summary>This interface should be implemented for every entity that needs to be rendered on screen</summary>
     public interface IAnimatable
     {
+        /// <summery>Wether this entity can perform an action or not</summary>
         bool CanPerform(Action a);
     }
 }
