fdb3bb8ae12a9eec4661b6854d7a6ac1070c1054
Eugenio Tampieri <eugenio@eutampieri.eu>
2021-05-07 09:52:28 +0200

    Documented IHealthModifier

--- Tampieri/Tampieri/IHealthModifier.cs
+++ Tampieri/Tampieri/IHealthModifier.cs
@@ -1,8 +1,11 @@
 ﻿namespace Tampieri.Model
 {
+    /// <summary>This interface describes an item that, if colliding with an entity, modifies its health</summary>
     public interface IHealthModifier
     {
+        /// <summary>The value to add to the entity's health</summary>
         int HealthDelta { get; }
+        /// <summary>Apply <c>this.HealthDelta</c> to the entity</summary>
         void UseOn(ILivingCharacter character)
         {
             int currentHealth = character.Health += this.HealthDelta;
